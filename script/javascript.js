9a356be225fb8318074285a6cdb11cdcee22852b
qiubee <45637654+qiubee@users.noreply.github.com>
2019-01-27 22:08:22 +0100

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -1,27 +1,24 @@
-// Variabelen
 var zoekBalk = document.querySelector('input[type="search"]');
 var navigatieBlok = document.querySelector('nav');
 var inlogKnop = document.querySelector('nav a');
 
+var selecteerJaar = document.querySelector('body > footer p span');
+var huidigJaartal = new Date().getFullYear();
+selecteerJaar.textContent = huidigJaartal;
 
-// Zet het type attribuut van zoekknop op button
+
 var zoekAttribuut = document.querySelector('header button[type="submit"]');
 zoekAttribuut.setAttribute('type', 'button');
 
 
-// Voegt class toe of verwijdert class voor zoekbalk, navigatieblok en inloglink na klik op zoekknop
 var zoekKnop = document.querySelector('header button[type="button"]');
 zoekKnop.addEventListener('click', function () {
-    //var zoekBalk = document.querySelector('input[type="search"]');
     zoekBalk.classList.toggle('zoeken');
-
-    //var navigatieBlok = document.querySelector('nav');
-    //var inlogKnop = document.querySelector('nav a');
     navigatieBlok.classList.toggle('verborgen');
     inlogKnop.classList.toggle('verborgen');
     });
 
-// Verwijdert class voor zoekbalk, navigatieblok en inloglink na klik buiten tekstveld
+
 function removeClass() {
     zoekBalk.classList.remove('zoeken');
     navigatieBlok.classList.remove('verborgen');
@@ -33,7 +30,6 @@ document.querySelector('body > main').addEventListener('click', removeClass);
 document.querySelector('body > footer').addEventListener('click', removeClass);
 
 
-// Bij meer dan 1 karakter in zoekbalk verandert zoekknop weer terug naar submitknop
 function checkInput() {
 var zoekVeldLengte = document.querySelector('input[type="search"]').value.length;
 
@@ -44,11 +40,8 @@ var zoekVeldLengte = document.querySelector('input[type="search"]').value.length
     else {
         zoekAttribuut.setAttribute('type', 'button');
     }
-} // Waarde checken van zoekveld
+}
 zoekBalk.addEventListener('keyup', checkInput);
 
 
-// Zet het jaartal naar huidige tijd
-var selecteerJaar = document.querySelector('body > footer p span');
-var huidigJaartal = new Date().getFullYear();
-selecteerJaar.textContent = huidigJaartal;
+
