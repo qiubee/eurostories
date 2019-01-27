76dec5a083414f7b1824cef240d6baa27eb690a9
qiubee <45637654+qiubee@users.noreply.github.com>
2019-01-27 22:08:58 +0100

    Add moe.js

---
+++ script/moe.js
@@ -0,0 +1,45 @@
+document.querySelector('main > article > div p:first-of-type span:first-child').addEventListener('click', function () {
+    document.querySelector('main > article > div p:first-of-type span:first-child').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:first-of-type span:last-child').addEventListener('click', function () {
+    document.querySelector('main > article > div p:first-of-type span:last-child').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(2) span:first-child').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(2) span:first-child').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(2) span:nth-child(2)').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(2) span:nth-child(2)').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(2) span:nth-child(3)').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(2) span:nth-child(3)').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(2) span:last-child').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(2) span:last-child').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(3) span:first-child').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(3) span:first-child').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(3) span:nth-child(2)').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(3) span:nth-child(2)').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(3) span:nth-child(3)').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(3) span:nth-child(3)').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:nth-of-type(3) span:last-child').addEventListener('click', function () {
+    document.querySelector('main > article > div p:nth-of-type(3) span:last-child').classList.toggle('open');
+});
+
+document.querySelector('main > article > div p:last-of-type span:first-child').addEventListener('click', function () {
+    document.querySelector('main > article > div p:last-of-type span:first-child').classList.toggle('open');
+});
+
+
